7fb6e77a7d14a1a82b4c6ac1e1c808e6f2400f71
Oma-yma1 <zgani.omayma@gmail.com>
2023-03-17 18:48:22 -0700

    task2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - program that prints the alphabet in lowercase
+ * Return: 0 success
+ */
+int main(void)
+{
+char i;
+for (i = 'a' ; i <= 'z' ; i++)
+putchar (i);
+for (i = 'A' ; i <= 'Z' ; i++)
+putchar (i);
+putchar ('\n');
+return (0);
+}
