90da5961fc651d65435157f4d32e41dee07621cc
Oma-yma1 <zgani.omayma@gmail.com>
2023-03-21 11:03:38 -0700

    task2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+char j;
+int i = 0;
+while (i <= 9)
+{
+for (j = 'a' ; j <= 'z' ; j++)
+_putchar(j);
+_putchar('\n');
+i++;
+}
+}
