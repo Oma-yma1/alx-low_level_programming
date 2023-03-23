2a2d1b46644d443af5cdb085cff621c52c5cbe43
Oma-yma1 <zgani.omayma@gmail.com>
2023-03-23 15:30:47 -0700

    task4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_most_numbers -  prints the numbers, from 0 to 9
+ * Return: numbers from 0 to 9  excluding 2 and 4
+ */
+void print_most_numbers(void)
+{
+int a = 0;
+for (;a <= 9; a++)
+{
+if (a == 2 || a == 4)
+{
+continue;
+}
+else
+{
+_putchar(a + '0');
+}
+}
+_putchar('\n');
+}
