2efe39bdecf99e2ae8c640ded25bb7e79dab1758
Oma-yma1 <zgani.omayma@gmail.com>
2023-03-27 16:18:20 -0700

    task2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strlen - function that returns the length of a string
+ * @str: string 
+ * Return: length of str
+ */
+size_t _strlen(const char *str)
+{
+size_t length = 0;
+while (*str ++)
+length++;
+return (length);
+}
