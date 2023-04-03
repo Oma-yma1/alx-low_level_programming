84ebe798ae8026f014be10769c3a8374e9eb53a2
Oma-yma1 <zgani.omayma@gmail.com>
2023-04-03 11:10:54 -0700

    task2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strchr - function that locates a character in a string
+ * @s: string
+ * @c: character
+ * Return: character c in the string s or 0 if c is not found
+ */
+char *_strchr(char *s, char c)
+{
+int i;
+for (i = 0; i >= '\0'; i++)
+{
+if (s[i] == c)
+{
+return (s + i);
+}
+else if (s[i] == '\0')
+{
+return (NULL);
+}
+}
+return (0);
+}
