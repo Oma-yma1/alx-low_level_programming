6731652fc662749818177f126ec6036b10058d07
Oma-yma1 <zgani.omayma@gmail.com>
2023-04-03 17:35:16 -0700

    task8

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * print_diagsums - the sum of the two diagonals of a square matrix of integers
+ * @a: array
+ * @size: size
+ * Return: alawys 0
+ */
+void print_diagsums(int *a, int size)
+{
+
