d2394d14886e3fb402c1d872999649e2ac971c76
Oma-yma1 <zgani.omayma@gmail.com>
2023-04-05 12:04:32 -0700

    task2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strlen_recursion - function that returns the length of a string
+ * @s: string
+ * Return: lenght of dtring
+ */
+int _strlen_recursion(char *s)
+{
+int len = 0;
+if (*s != '\0')
+{
+len++;
+len += _strlen_recursion(s+1);
+}
+return (len);
+}
