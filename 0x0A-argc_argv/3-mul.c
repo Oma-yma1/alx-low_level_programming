943b1524195c08e805d99d35adfe8e29d9546ba4
Oma-yma1 <zgani.omayma@gmail.com>
2023-04-08 10:09:22 -0700

    task3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - program that multiplies two numbers
+ * @argc: arg count
+ * @argv: arg vector
+ * Return: 0 success
+ */
+int main(int argc, char *argv[])
+{
+int a, b;
+if (argc < 3)
+{
+printf("Error\n");
+return (1);
+}
+else
+{
+a = atoi(argv[1]);
+b = atoi(argv[2]);
+printf("%d\n", a * b);
+return (0);
+}
+}
