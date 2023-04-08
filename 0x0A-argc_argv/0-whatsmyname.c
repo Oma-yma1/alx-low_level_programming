d8a97f5be34a03ac0c8e86ac08488f736580a971
Oma-yma1 <zgani.omayma@gmail.com>
2023-04-08 09:27:29 -0700

    task0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main -  program that prints its name, followed by a new line
+ * @argc: arg count
+ * @argc: arg vector
+ * Return: 0 success
+ */
+int main (int argc, char **argv)
+{
+(void) argc;
+printf("%s\n", argv[0]);
+return (0);
+}
