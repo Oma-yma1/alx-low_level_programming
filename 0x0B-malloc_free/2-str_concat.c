e61dc7ac445792bb35db558726c8f06a227d4d88
Oma-yma1 <zgani.omayma@gmail.com>
2023-04-11 13:16:02 -0700

    task2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -9,6 +9,23 @@
 char *str_concat(char *s1, char *s2)
 {
 char *concat;
-int i, j;
-
-
+int i, j, l1, l2;
+l1 = l2 = 0;
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
+while (s1[l1] != '\0')
+l1++;
+while (s2[l2] != '\0')
+l2++;
+concat = (char *)malloc(sizeof(char) * (l1 + l2 + 1));
+if (concat == NULL)
+return (NULL);
+for (i = 0; i < l1; i++)
+concat[i] = s1[i];
+for (j = 0; j < l2; j++, i++)
+concat[i] = s2[j];
+concat[l1 + l2] = '\0';
+return (concat);
+}
