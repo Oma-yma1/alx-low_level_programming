52b5b5e3c5dff33536001b4ac38198db3435c382
Oma-yma1 <zgani.omayma@gmail.com>
2023-04-11 11:55:47 -0700

    task1

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * str_concat -  function that concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: concatenation of s1 and s2
+ */
+char *str_concat(char *s1, char *s2)
+{
+char *concat;
+int i, j;
+
+
