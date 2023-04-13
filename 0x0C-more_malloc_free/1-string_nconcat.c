bbd428dca6153bd4f6ae71c4ef9c8e3f76eec7c0
Oma-yma1 <zgani.omayma@gmail.com>
2023-04-13 15:33:35 -0700

    task1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,9 +10,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 char *s;
 unsigned int i = 0, j = 0, k = 0;
-while (s1[i])
+while (s1 && s1[i])
 i++;
-while (s2[j])
+while (s2 && s2[j])
 j++;
 if (j > n)
 j = n;
