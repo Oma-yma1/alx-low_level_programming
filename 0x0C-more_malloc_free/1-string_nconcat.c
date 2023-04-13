28009adcf1a3e6cbf58fb17463f3c965461d673a
Oma-yma1 <zgani.omayma@gmail.com>
2023-04-13 15:09:29 -0700

    task1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,8 +10,24 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 char *s;
 unsigned int i = 0, j = 0, k = 0;
+if (s1 == NULL)
+{
+(i = 0);
+}
+else
+{
 for (i = 0; s1[i]; ++i)
+;
+}
+if (s2 == NULL)
+{
+j = 0;
+}
+else
+{
 for (j = 0; s2[j]; ++j)
+;
+}
 if (j >= n)
 s = malloc(sizeof(char) * (i + j + 1));
 if (s == NULL)
