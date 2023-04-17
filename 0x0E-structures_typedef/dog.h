d821ab69bf9f68107a3e653a487dca2f7de73b2c
Oma-yma1 <zgani.omayma@gmail.com>
2023-04-17 10:47:31 -0700

    task0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,10 +16,10 @@ char *owner;
  * dog_t - type
  */
 typedef struct dog dog_t;
-void init_dog(struct dog *d, char * name, float age, char *owner);
+void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *neew_dog(char *name, float age, char *owner);
-void free_dog(dog_t*d);
+void free_dog(dog_t *d);
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
 #endif
