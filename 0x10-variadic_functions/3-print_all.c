8d5901d684b03177e18669f0bbbfbbe145ac6e57
Oma-yma1 <zgani.omayma@gmail.com>
2023-04-20 12:09:56 -0700

    task3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+#include <stdarg.h>
+/**
+ * print_all - function that prints anything
+ * @format: type of arg
+ * @...: variable number
+ */
+void print_all(const char * const format, ...)
+{
+
