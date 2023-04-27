650ba3b7aac8fb15b92fa717379f4d39f6f33280
Oma-yma1 <zgani.omayma@gmail.com>
2023-04-27 13:31:04 -0700

    task6

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -2,8 +2,10 @@ global main
  extern printf
 main:
  mov edi, format
+ mov esi, message
  xor eax, eax
  call printf
  mov   eax, 0
  ret
-format: db 'Hello, Holberton\n', 0
+message: db "Hello, Holberton", 0
+format: db "%s", 10, 0
