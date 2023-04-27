f2d828422d21118e8c109ae6eba28a75f4ae0ac2
Oma-yma1 <zgani.omayma@gmail.com>
2023-04-27 12:53:51 -0700

    task5

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-void first()__attribute__((constructor));
+void first(void)__attribute__((constructor));
 /**
  * first - prints a sentence before the main function is excecuted
  */
-void first()
+void first(void)
 {
-printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
 }
