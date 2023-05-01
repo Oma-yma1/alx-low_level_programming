b5fa1e21db11f83c2650bdd4d951176f452c4375
Oma-yma1 <zgani.omayma@gmail.com>
2023-05-01 11:05:10 -0700

    task4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+/**
+ * free_listint - frees a listint_t list
+ * @head: pointer
+ */
+void free_listint(listint_t *head)
+{
+listint_t *t;
+for (t = 0; head != NULL; t = head->next)
+{
+free(head);
+head = t;
+}
+}
