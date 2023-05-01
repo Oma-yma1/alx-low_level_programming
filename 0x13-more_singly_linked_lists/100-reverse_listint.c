ad864d55112789d28bad45244affd0b36af37e12
Oma-yma1 <zgani.omayma@gmail.com>
2023-05-01 13:39:58 -0700

    task11

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * reverse_listint - reverses a listint_t
+ * @head: pointer
+ * Return: pointer to the first node
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+listint_t *next = NULL;
+listint_t *previous = NULL;
+while (*head != NULL)
+{
+next = (*head)->next;
+(*head)->next = previous;
+previous = *head;
+*head = next;
+}
+*head = previous;
+return (previous);
+}
