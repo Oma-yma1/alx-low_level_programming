35ac0eb239668b99fbfc552557e2406da80082e3
Oma-yma1 <zgani.omayma@gmail.com>
2023-05-04 11:08:49 -0700

    task2

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * get_bit - returns the value of a bit
+ * @n: unsigned long int
+ * @index: unsigned int
+ * Return: the value of the bit at indexindex or -1
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+if(index > 63)
+{
+return (-1);
+}
+else
+{
+return (n >> index) & 1;
+}
+}
