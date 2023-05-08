95479e2fac7fa64e3178460a4c6a6d3ff01c287f
Oma-yma1 <zgani.omayma@gmail.com>
2023-05-08 15:25:16 -0700

    task1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <string.h>
 #include <fcntl.h>
 /**
  * create_file - creats file
@@ -9,15 +8,14 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-size_t text_le;
-int fi, wi;
+int fi, wi, text_le;
 if (filename == NULL)
 return (-1);
 fi = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 if (fi == -1)
 return (-1);
-if (text_content != NULL)
-text_le = strlen(text_content);
+if (text_content == NULL)
+for (text_le = 0; text_content[text_le] != '\0'; text_le++)
 wi = write(fi, text_content, text_le);
 if (wi == -1)
 return (-1);
