6daf34bde07d75d8ae389f9cd1f5f1733ffaecbd
Oma-yma1 <zgani.omayma@gmail.com>
2023-05-08 16:03:29 -0700

    task2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,7 +11,7 @@ int append_text_to_file(const char *filename, char *text_content)
 int fi, wi, len = 0;
 if (filename == NULL)
 return (-1);
-if (text_content == NULL)
+if (text_content != NULL)
 {
 for (; text_content[len];)
 len++;
