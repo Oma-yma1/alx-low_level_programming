a097f69caf08424fe802b014742f90d2ebfc07ce
Oma-yma1 <zgani.omayma@gmail.com>
2023-05-08 15:43:19 -0700

    task0

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,16 +8,16 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-int fi, wi, le;
+int fi, wi, len = 0;
 if (filename == NULL)
 return (-1);
 if (text_content != NULL)
 {
-for (le = 0; text_content[le];)
-le++;
+for (len = 0; text_content[len];)
+len++;
 }
 fi = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-wi = write(fi, text_content, le);
+wi = write(fi, text_content, len);
 if (fi == -1 || wi == -1)
 return (-1);
 close(fi);
