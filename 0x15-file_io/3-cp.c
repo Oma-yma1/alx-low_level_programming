58e44b5826ea969f13ae396342917c9e6fdf12ec
Oma-yma1 <zgani.omayma@gmail.com>
2023-05-08 18:53:20 -0700

    task3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -10,14 +10,14 @@ void fi_closing(int fd);
  */
 char *buff_creat(char *file)
 {
-char *buff;
-buff = malloc(sizeof(char) * 1024);
-if (buff == NULL)
+char *buffer;
+buffer = malloc(sizeof(char) * 1024);
+if (buffer == NULL)
 {
 dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
 exit(99);
 }
-return (buff);
+return (buffer);
 }
 /**
  * fi_closing - close file
@@ -43,34 +43,34 @@ exit(100);
 int main(int argc, char *argv[])
 {
 int file_from, file_to, re, wr;
-char *buff;
+char *buffer;
 if (argc != 3)
 {
 dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 exit(97);
 }
-buff = buff_creat(argv[2]);
+buffer = buff_creat(argv[2]);
 file_from = open(argv[1], O_RDONLY);
-re = read(file_from, buff, 1024);
+re = read(file_from, buffer, 1024);
 file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 do {
 if (file_from == -1 || re == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-free(buff);
+free(buffer);
 exit(98);
 }
-wr = write(file_to, buff, re);
+wr = write(file_to, buffer, re);
 if (file_to == -1 || wr == -1)
 {
 dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-free(buff);
+free(buffer);
 exit(99);
 }
-re = read(file_from, buff, 1024);
+re = read(file_from, buffer, 1024);
 file_to = open(argv[2], O_WRONLY | O_APPEND);
 } while (re > 0);
-free(buff);
+free(buffer);
 fi_closing(file_from);
 fi_closing(file_to);
 return (0);
