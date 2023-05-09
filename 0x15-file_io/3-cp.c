7092d3d7c144d6b7a9e0028451ed299d32a534d1
Oma-yma1 <zgani.omayma@gmail.com>
2023-05-08 18:22:58 -0700

    task3

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,75 @@
+#include "main.h"
+#include <stdio.h>
+#include <fcntl.h>
+char *buff_creat(char *fi);
+void fi_closing(int fc);
+/**
+ * buff_creat - creat buffer
+ * @fi: file
+ * Return: buffer
+ */
+char *buff_creat(char *fi)
+{
+char *buff;
+buff = malloc(sizeof(char) *1024);
+if (buff == NULL)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", fi);
+exit(99);
+}
+return (buff);
+}
+/**
+ * fi_closing - close file
+ * @fi1
+ */
+void fi_closing(int fd)
+{
+if (close(fd) == -1)
+{
+dprintf(STDERR_FILENO, "Error/ Can't close fd %d\n", fd);
+exit(100);
+}
+}
+/**
+ * main - copies the content of a file ti onother file
+ *
+ * @argv: pointer
+ * @argc: argument
+ * Return: 0 always
+ */
+int main(int argc, char *argv[])
+{
+int file_from, file_to, re, wr;
+char *buff;
+if (argc != 3)
+{
+dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+exit(97);
+}
+buff = buff_creat(argv[2]);
+file_from = open(argv[1], O_RDONLY);
+re = read(file_from, buff, 1024);
+file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+do{
+if (file_from == -1 || re == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+free(buff);
+exit(98);
+}
+wr = write(file_to, buff, re);
+if (file_to == -1 || wr == -1)
+{
+dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+free(buff);
+exit(99);
+}
+re = read(file_from, buff, 1024);
+file_to = open(argv[2], O_WRONLY | O_APPEND);
+}while (re > 0);
+free(buff);
+fi_closing(file_from);
+fi_closing(file_to);
+return (0);
+}
