04251f5d38443ccd0c3943ca781fc71702d5dc69
Oma-yma1 <zgani.omayma@gmail.com>
2023-07-06 09:27:51 -0700

    hash tables

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -5,19 +5,19 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
-hash_node_t *node;
-unsigned long int j;
+hash_node_t *nod;
+unsigned long int j = 0;
 if (ht == NULL)
 return;
 while (j < ht->size)
 {
 while (ht->array[j] != NULL)
 {
-node = ht->array[j]->next;
+nod = ht->array[j]->next;
 free(ht->array[j]->key);
 free(ht->array[j]->value);
 free(ht->array[j]);
-ht->array[j] = node;
+ht->array[j] = nod;
 }
 j++;
 }
