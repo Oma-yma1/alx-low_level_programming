df77a81034a6478cf6fd8439c6f1797477a4a68b
Oma-yma1 <zgani.omayma@gmail.com>
2023-10-10 12:55:26 -0700

    task

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -14,24 +14,20 @@ int s = (int)sqrt(size);
 int p = 0;
 if (array == NULL)
 return (-1);
-
 printf("Value checked array[0] = [%d]\n", array[0]);
 
 while (array[s] < value && s < (int)size)
 {
 printf("Value checked array[%d] = [%d]\n", s, array[s]);
 p = s;
-s += sqrt(size);
+s += (int)sqrt(size);
 }
-
 printf("Value found between indexes [%d] and [%d]\n", p, s);
-
 for (i = p; i <= s && i < (int)size; i++)
 {
 printf("Value checked array[%d] = [%d]\n", i, array[i]);
 if (array[i] == value)
 return (i);
 }
-
 return (-1);
 }
